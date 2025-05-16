1dc9bddb9fb99b2072cee2a1a64149d7101a1380
PrathamDesai07 <prathamdesai071204@gmail.com>
2025-05-16 15:56:12 +0530

    1. Model push

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# RPG_SearchEngine
-RPG based smart search engine
+# Easy Webpage Summarizer
+
+A Python script designed to summarize webpages from specified URLs using the LangChain framework and the ChatOllama model. It leverages advanced language models to generate detailed summaries, making it an invaluable tool for quickly understanding the content of web-based documents.
+
+## Requirements
+
+[ollama](https://ollama.com/) must be installed and served
+
+```bash
+ollama run llama3:instruct
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+## Features
+
+- Summarization of webpages and youtube videos directly from URLs.
+- Translates to Turkish language (other languages will be added soon!)
+- Integration with LangChain and ChatOllama for state-of-the-art summarization.
+- Command-line interface for easy use and integration into workflows.
+
+## Usage
+
+To use the webpage summarizer, run the script from the command line, providing the URL of the document you wish to summarize:
+
+```bash
+python summarizer.py -u "http://example.com/document"
+```
+
+Replace `http://example.com/document` with the actual URL of the document you want to summarize.
+
+### Web UI
+
+To use the webpage summarizer in you web browser, you can also try gradio app.
+
+```bash
+python app/webui.py
+```
+
+![gradio](assets/gradio.png)
+
+## Docker
+
+```bash
+docker build -t web_summarizer .
+docker run -p 7860:7860 web_summarizer
+
+# Run if you run ollama on host
+docker run -d --network='host' -p 7860:7860 web_summarizer
+```
+
+
+## Development
+
+To contribute to the development of this script, clone the repository, make your changes, and submit a pull request. We welcome contributions that improve the script's functionality or extend its capabilities.
+
+- [x] Summarize youtube videos
+- [x] Dockerize project
+- [ ] Translate to different languages
+- [ ] Streaming text output on gradio
+- [ ] Serve on web
+
+## License
+
+This script is released under the MIT License. See the [LICENSE](./LICENSE) file in the repository for full details.
---
+++ app/summarizer.py
@@ -0,0 +1,62 @@
+import argparse
+
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_community.document_loaders import WebBaseLoader
+
+
+def setup_argparse():
+    """Setup argparse to parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Summarize a document from a given URL."
+    )
+    parser.add_argument(
+        "-u", "--url", required=True, help="URL of the document to summarize"
+    )
+    return parser.parse_args()
+
+
+def load_document(url):
+    """Load document from the specified URL."""
+    loader = WebBaseLoader(url)
+    return loader.load()
+
+
+def setup_summarization_chain():
+    """Setup the summarization chain with a prompt template and ChatOllama."""
+    prompt_template = PromptTemplate(
+        template="""As a professional summarizer, create a detailed and comprehensive summary of the provided text, be it an article, post, conversation, or passage, while adhering to these guidelines:
+            1. Craft a summary that is detailed, thorough, in-depth, and complex, while maintaining clarity.
+
+            2. Incorporate main ideas and essential information, eliminating extraneous language and focusing on critical aspects.
+
+            3. Rely strictly on the provided text, without including external information.
+
+            4. Format the summary in paragraph form for easy understanding.
+
+            5.Conclude your notes with [End of Notes, Message #X] to indicate completion, where "X" represents the total number of messages that I have sent. In other words, include a message counter where you start with #1 and add 1 to the message counter every time I send a message.
+
+        By following this optimized prompt, you will generate an effective summary that encapsulates the essence of the given text in a clear, detailed, and reader-friendly manner. Optimize output as markdown file.
+
+        "{text}"
+
+        DETAILED SUMMARY:""",
+        input_variables=["text"],
+    )
+
+    llm = ChatOllama(model="llama3:instruct", base_url="http://127.0.0.1:11434")
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template)
+    return llm_chain
+
+
+def main():
+    args = setup_argparse()
+    docs = load_document(args.url)
+
+    llm_chain = setup_summarization_chain()
+    result = llm_chain.run(docs)
+
+
+if __name__ == "__main__":
+    main()
